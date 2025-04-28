300677475871ccf668220029ef20e63ebfd12943
Fabricio Gabriel <floyd9732@gmail.com>
2025-04-28 06:03:24 -0300

    feat: updating background

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -3,7 +3,7 @@ const Background = () => {
     <div 
       className="absolute inset-0 bg-cover bg-center bg-no-repeat" 
       style={{ 
-        backgroundImage: "url('https://images.unsplash.com/photo-1744951482542-7d7fcba214c8?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
+        backgroundImage: "url('https://images.unsplash.com/photo-1743485532768-9a5e07a2f666?q=80&w=3889&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')"
       }}
     >
       {/* Black overlay with low opacity */}
